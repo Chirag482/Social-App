6914cfa4b19d55b269254d3d39478cd47fa3fa02
Chirag <chiragvalecha97@gmail.com>
2021-11-15 12:24:13 +0530

    add redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
 import App from "./components/App";
+import { configureStore } from "./store";
 
+const store = configureStore();
+console.log("store", store);
 ReactDOM.render(<App />, document.getElementById("root"));
